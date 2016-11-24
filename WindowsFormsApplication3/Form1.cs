17d5d1f01c3e95dbb9684fc43c7982c4ce099fcb
zbyclar <zbyclar@gmail.com>
2016-11-24 10:18:31 +0800

    add text chat function as a trial
    when testing on one pc for both sender and receiver, please set to ports
    first on sender side, then reverse the order of ports and set them on
    receiver side.

--- WindowsFormsApplication3/Form1.cs
+++ WindowsFormsApplication3/Form1.cs
@@ -2,15 +2,79 @@
 using System.Windows.Forms;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 namespace WindowsFormsApplication3
 {
     [System.Runtime.InteropServices.ComVisibleAttribute(true)]
     public partial class Form1 : Form
     {
+        Socket sck;
+        EndPoint epLocal, epRemote;
        
         public Form1()
         {
+
             InitializeComponent();
+
+            //Set up IP connection between epLocal and epRemote
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            //get local IP address, will be replaced by real-machine ip address in the future
+            GlobalVariable.localIp = GetLocalIP();
+            textBox1.Text = GetLocalIP();
+            textBox2.Text = GetLocalIP();
+            
+        }
+
+        
+        private String GetLocalIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+
+            foreach(IPAddress ip in host.AddressList)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+
+            }
+
+            return "127.0.0.1";
+        }
+        
+        private void MessageCallBack(IAsyncResult aResult)
+        {
+            try
+            {
+                int size = sck.EndReceiveFrom(aResult, ref epRemote);
+
+                //there must be an epRemote to get message from
+                if (size > 0)
+                {
+                    byte[] receivedData = new byte[1464];
+                    receivedData = (byte[])aResult.AsyncState;
+                    ASCIIEncoding eEncoding = new ASCIIEncoding();
+                    string receivedMessage = eEncoding.GetString(receivedData);
+
+                    listBox1.Items.Add("Friends: " + receivedMessage);
+                }
+
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+
+
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -37,8 +101,6 @@ namespace WindowsFormsApplication3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            webBrowser1.Navigate(new Uri("c:/users/zbyclar/documents/visual studio 2015/Projects/WindowsFormsApplication3/WindowsFormsApplication3/BaiduMap.html"));
-            
             
         }
 
@@ -91,6 +153,67 @@ namespace WindowsFormsApplication3
                 }
             }
             
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+                byte[] msg = new byte[1500];
+                msg = enc.GetBytes(textBox6.Text);
+
+                sck.Send(msg);
+                listBox1.Items.Add("You: "+textBox6.Text);
+                textBox6.Clear();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            epLocal = new IPEndPoint(IPAddress.Parse(textBox1.Text), Convert.ToInt32(textBox3.Text));
+            sck.Bind(epLocal);
+            Console.WriteLine("bind local ip successfully !");
+
+            epRemote = new IPEndPoint(IPAddress.Parse(textBox2.Text), Convert.ToInt32(textBox4.Text));
+            sck.Connect(epRemote);
+
+            byte[] buffer = new byte[1500];
+            sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+            button4.Enabled = false;
+            button4.Text = "Connected";
+            Submit.Enabled = true;
+            textBox6.Focus();
         }
 
         private void panel3_Paint(object sender, PaintEventArgs e)
