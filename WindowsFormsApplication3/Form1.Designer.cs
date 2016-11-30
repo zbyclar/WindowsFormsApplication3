4fbd7b3e36ae83c6bad39a904a5e3eefe300a9a8
zbyclar <zbyclar@gmail.com>
2016-11-30 16:24:43 +0800

    add voice chat
    add voice chat function, as well as creating document transfer interface

--- WindowsFormsApplication3/Form1.Designer.cs
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -80,6 +80,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "语音聊天";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // webBrowser1
             // 
